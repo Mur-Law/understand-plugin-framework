7f0e4ad4b150ccf8ed6b8ef76c0fdc1628f44519
Mur-Law <40891212+Mur-Law@users.noreply.github.com>
2025-07-11 10:56:36 +0800

    Update HookHelper.java
    添加10以上代理ams

--- ams-pms-hook/src/main/java/com/weishu/upf/ams_pms_hook/app/HookHelper.java
+++ ams-pms-hook/src/main/java/com/weishu/upf/ams_pms_hook/app/HookHelper.java
@@ -12,6 +12,76 @@ import android.content.pm.PackageManager;
  * @date 16/3/7
  */
 public final class HookHelper {
+    //这个测试在安卓10到13还可以使用
+     public static void hookActivityManager_android10() {
+try {
+            //取消 反射限制 
+            Class<?> vmRuntimeClass = Class.forName("dalvik.system.VMRuntime");
+            Method getRuntimeMethod = vmRuntimeClass.getDeclaredMethod("getRuntime");
+            Object vmRuntime = getRuntimeMethod.invoke(null);
+            Method setHiddenApiExemptionsMethod = vmRuntimeClass.getDeclaredMethod(
+                    "setHiddenApiExemptions", String[].class);
+            setHiddenApiExemptionsMethod.invoke(vmRuntime, new Object[]{new String[]{"L"}});
+
+
+
+            Class<?> ActivityTaskManagerclass = Class.forName("android.app.ActivityTaskManager");
+            // 在安卓13上 获取 IActivityTaskManagerSingletonField 这个字段, 想办法替换它
+            Field IActivityTaskManagerSingletonField = ActivityTaskManagerclass.getDeclaredField("IActivityTaskManagerSingleton");
+            IActivityTaskManagerSingletonField.setAccessible(true);
+            //获取静态变量
+            Object IActivityTaskManagerSingleton = IActivityTaskManagerSingletonField.get(null);
+
+            // 10以上是用的IActivityTaskManagerSingleton， android.util.Singleton对象; 我们取出这个单例里面的字段
+            Class<?> singleton = Class.forName("android.util.Singleton");
+            Field mInstanceField = singleton.getDeclaredField("mInstance");
+            mInstanceField.setAccessible(true);
+
+            // ActivityTaskManager IActivityTaskManagerSingleton 获取系统的 IActivityManager对象
+           // Object rawIActivityManager  = mInstanceField.get(IActivityTaskManagerSingleton);
+            // 2. 通过 ServiceManager 获取 ACTIVITY_TASK_SERVICE 的 IBinder
+            Class<?> serviceManagerClass = Class.forName("android.os.ServiceManager");
+            Method getServiceMethod = serviceManagerClass.getDeclaredMethod("getService", String.class);
+            getServiceMethod.setAccessible(true);
+            // "activity_task" 是 ACTIVITY_TASK_SERVICE 的服务名（Context.ACTIVITY_TASK_SERVICE 的值）
+            IBinder activityTaskBinder = (IBinder) getServiceMethod.invoke(null, "activity_task");
+
+            // 3. 将 IBinder 转换为 IActivityTaskManager 实例（通过 Stub.asInterface）
+            Class<?> iTaskManagerStubClass = Class.forName("android.app.IActivityTaskManager$Stub");
+            @SuppressLint("BlockedPrivateApi") Method asInterfaceMethod = iTaskManagerStubClass.getDeclaredMethod("asInterface", IBinder.class);
+            asInterfaceMethod.setAccessible(true);
+            Object rawIActivityTaskManager = asInterfaceMethod.invoke(null, activityTaskBinder);
+
+
+            if (rawIActivityTaskManager == null) {
+                Log.e("hhhh HookError", "原始 IActivityTaskManager 实例为 null！");
+            }else {
+                Log.e("hhhh HookError", "原始 IActivityTaskManager 实例为！" + rawIActivityTaskManager);
+            }
+            // 创建一个这个对象的代理对象, 然后替换这个字段, 让我们的代理对象帮忙干活
+            Class<?> iActivityManagerInterface = Class.forName("android.app.IActivityTaskManager");
+            Object proxy = Proxy.newProxyInstance(
+                    Thread.currentThread().getContextClassLoader(),
+                    new Class<?>[]{iActivityManagerInterface},
+                    new HookHandler(rawIActivityTaskManager));
+                    mInstanceField.set(IActivityTaskManagerSingleton,proxy);
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+         
+     }
+
+    
     //这个获取实例方法在安卓10到13已经 用不了了
     public static void hookActivityManager_android4() {  
         try {
@@ -42,6 +112,7 @@ public final class HookHelper {
         }
 
     }
+
 
     public static void hookPackageManager(Context context) {
         try {
